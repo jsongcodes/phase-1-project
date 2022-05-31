4a0583aa5bc2f2c586c15e61a8b2c6b5a74c5aa8
Jasmin <ssongnari@gmail.com>
2022-05-31 13:56:33 -0400

    added event listener to submit

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const catForm = document.querySelector('#cat-form');
+document.addEventListener('DOMContentLoaded', () => {
+    const catForm = document.querySelector('#cat-form');
 
-catForm.addEventListener('submit', newCat);
+    catForm.addEventListener('submit', newCat);
 
-function newCat(e) {
-    e.preventDefault();
-}
+    function newCat(e) {
+        e.preventDefault();
+
+        const name = document.querySelector('#name-input').value;
+        const img = document.querySelector('#img-input').value;
+        const age = document.querySelector('#age-input').value;
+        const description = document.querySelector('#description-input').value;
+
+        let newCatObj = {
+            name: name,
+            img: img,
+            age: age,
+            description: description,
+            likes: 0
+        }
+
+        fetch(`http://localhost:8000/cats`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(newCatObj)
+        })
+            .then(resp => resp.json())
+            .then(({ name }) => { console.log(name) })
+
+        renderCat(newCatObj);
+        catForm.reset;
+    }
+})
