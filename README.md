cc95ab497caa3e3f364c9951a0cc76b6823142cf
Jasmin <ssongnari@gmail.com>
2022-06-03 14:20:26 -0400

    updated db json

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Phase 1 Project
 * This adoptable cats page is to help cats who need homes. Many cats are euthanized a year due to lack of homes and space in shelters. This page would allow users to see a list of adoptable cats to help with this problem. This page shows users a list of adoptable cats. Each card lists the cat's image, age, name, description, and number of likes. Users are able to like the cats they see. They are also able to add their own adoptable cats onto the page. Once the cats are adopted, their card would be deleted so that they are no longer showing on the page. 
 
+* I utilized ____ 
+
+* I implemented CSS to help ___
+
 ### Requirements
 * Have json-server installed
 
@@ -8,3 +12,6 @@
 * Fork and clone
 * Run json server for db.json
 * Open index.html in the browser
+
+### Credits
+*
--- index.js
+++ index.js
@@ -1,109 +1,118 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const baseUrl = 'http://localhost:3000'
-    const catForm = document.querySelector('#cat-form');
-    const catContainer = document.querySelector('#cat-container');
-    const catFormContainer = document.querySelector('#cat-form-container');
 
-    catForm.addEventListener('submit', newCat);
 
-    function newCat(e) {
+const baseUrl = 'http://localhost:3000'
+const catForm = document.querySelector('#cat-form');
+const catContainer = document.querySelector('#cat-container');
+const catFormContainer = document.querySelector('#cat-form-container');
+
+catForm.addEventListener('submit', newCat);
+
+function newCat(e) {
+    e.preventDefault();
+
+    const name = document.querySelector('#name-input').value;
+    const img = document.querySelector('#img-input').value;
+    const age = document.querySelector('#age-input').value;
+    const description = document.querySelector('#description-input').value;
+
+    let catObj = {
+        name,
+        img,
+        age,
+        description,
+        likes: 0
+    }
+
+    fetch(`${baseUrl}/cats`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(catObj)
+    })
+        .then(resp => resp.json())
+        .then(newCatObj => renderCat(newCatObj))
+    catForm.reset;
+}
+function getCat() {
+    fetch(`${baseUrl}/cats`)
+        .then(resp => resp.json())
+        .then(catsArr => catsArr.forEach(cat => renderCat(cat)));
+}
+
+function renderCat(cat) {
+    const catCard = document.createElement('div');
+    catCard.id = `cat-${cat.id}`;
+    catCard.className = 'cat-card';
+
+    // catCard.addEventListener('click', () => (cat));
+
+    const catImg = document.createElement('img');
+    catImg.src = cat.img;
+    catImg.alt = `${cat.name} image`;
+
+    const catName = document.createElement('h3');
+    catName.textContent = `${cat.name}`;
+
+    const catAge = document.createElement('p');
+    catAge.textContent = `Age: ${cat.age}`;
+
+    const catDescription = document.createElement('p');
+    catDescription.textContent = `Description: ${cat.description}`;
+
+    const catLikes = document.createElement('p');
+    catLikes.textContent = `Likes: `;
+
+    const likesNum = document.createElement('p');
+    likesNum.className = 'likes-num';
+    likesNum.textContent = cat.likes;
+
+    const likesBttn = document.createElement('button');
+    likesBttn.className = 'likes-bttn';
+    likesBttn.textContent = '♥';
+
+    likesBttn.addEventListener('click', (e) => {
         e.preventDefault();
-
-        const name = document.querySelector('#name-input').value;
-        const img = document.querySelector('#img-input').value;
-        const age = document.querySelector('#age-input').value;
-        const description = document.querySelector('#description-input').value;
-
-        let newCatObj = {
-            name,
-            img,
-            age,
-            description,
-            likes: 0
-        }
-
-        fetch(`${baseUrl}/cats`, {
-            method: 'POST',
+        ++cat.likes;
+        const newLikes = { likes: cat.likes }
+        likesNum.textContent = cat.likes;
+        fetch(`${baseUrl}/cats/${cat.id}`, {
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
-            body: JSON.stringify(newCatObj)
+            body: JSON.stringify(newLikes)
         })
-        renderCat(newCatObj);
-        catForm.reset;
-    }
-    function getCat() {
-        fetch(`${baseUrl}/cats`)
             .then(resp => resp.json())
-            .then(catsArr => catsArr.forEach(cat => renderCat(cat)));
-    }
-    getCat();
-
-    function renderCat(cat) {
-        const catCard = document.createElement('div');
-        catCard.id = `cat-${cat.id}`;
-        catCard.className = 'cat-card';
-
-        catCard.addEventListener('click', () => (cat));
-
-        const catImg = document.createElement('img');
-        catImg.src = cat.img;
-        catImg.alt = `${cat.name} image`;
-
-        const catName = document.createElement('h3');
-        catName.textContent = `${cat.name}`;
-
-        const catAge = document.createElement('p');
-        catAge.textContent = `Age: ${cat.age}`;
-
-        const catDescription = document.createElement('p');
-        catDescription.textContent = `Description: ${cat.description}`;
-
-        const catLikes = document.createElement('p');
-        catLikes.textContent = `Likes: `;
-
-        const likesNum = document.createElement('p');
-        likesNum.className = 'likes-num';
+            .then((cat) => likesNum.textContent = cat.likes)
         likesNum.textContent = cat.likes;
+    })
 
-        const likesBttn = document.createElement('button');
-        likesBttn.className = 'likes-bttn';
-        likesBttn.textContent = '♥';
-
-        likesBttn.addEventListener('click', (e) => {
-            e.stopPropagation();
-            ++cat.likes;
-            const newLikes = { likes: cat.likes }
-            likesNum.textContent = cat.likes;
-            fetch(`${baseUrl}/cats/${cat.id}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify(newLikes)
-            })
-            likesNum.textContent = cat.likes;
-        })
-
-        const deleteBttn = document.createElement('button');
-        deleteBttn.className = 'delete-bttn';
-        deleteBttn.textContent = 'Delete me! I\'ve been adopted!'
-        deleteBttn.addEventListener('click', (e) => {
-            e.stopPropagation();
-            fetch(`${baseUrl}/cats/${cat.id}`), {
-                method: 'DELETE'
+    const deleteBttn = document.createElement('button');
+    deleteBttn.className = 'delete-bttn';
+    deleteBttn.textContent = 'Delete me! I\'ve been adopted!'
+    deleteBttn.addEventListener('click', (e) => {
+        e.preventDefault();
+        fetch(`${baseUrl}/cats/${cat.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
-                .then(resp => resp.json())
-                .then(() => { catCard.remove() })
         })
+            .then(resp => resp.json())
+            .then(data => catCard.remove())
+    })
 
-        catCard.append(catImg, catName, catAge, catDescription, catLikes, likesNum, likesBttn, deleteBttn);
+    catCard.append(catImg, catName, catAge, catDescription, catLikes, likesNum, likesBttn, deleteBttn);
 
-        catContainer.appendChild(catCard);
+    catContainer.appendChild(catCard);
 
-        return catCard;
-    }
+    return catCard;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getCat();
 })
-
